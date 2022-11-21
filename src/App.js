055f5bd79d8fe98ace9f3a5c72a281c75f0948b4
Himanku Gogoi <himankugogoi5@gmail.com>
2022-11-21 21:03:02 +0530

    created the link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My Portfolio</h2>
+      <h2>Part-I</h2>
     </div>
   );
 }
